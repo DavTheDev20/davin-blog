aafee787308b061ce7011a49104149f1a110bb31
DavTheDev20 <reiddavin@outlook.com>
2021-08-16 18:37:51 -0400

    Add postgres binaries and changed sqlite server to postgres server uri

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
+
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
